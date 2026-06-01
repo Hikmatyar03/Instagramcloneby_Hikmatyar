bad20fb11fd88fef1ad7939ff8a9697eef22238d
Hikmatyar <busssnies@hikmatyar.site>
2026-06-01 22:12:07 +0500

    Buges fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+ import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthStore } from './store/authStore';
 import { connectSocket } from './api/socket';
